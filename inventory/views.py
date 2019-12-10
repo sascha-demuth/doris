7e3759bbdaca3c2f01f5708d81935c107fff6177
Sascha Demuth <sascha.demuth@web.de>
2019-12-10 12:25:10 +0100

    Das Geruest fuer die Aplikation steht, dies muss jetzt mit leben gefuellt werden

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import SingleArticle, Category, ArticleDetails, ArticleComments
+
 
 # Create your views here.
+def cat_list(request):
+    categorys = Category.objects.all()
+    return render(request, 'inventory/cat_list.html', {'categorys': categorys})
+
